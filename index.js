774d93c79b4add827d0f8091cef4c23cea8987de
Gaby <gabriela.noriega@alumnos.udg.mx>
2023-10-19 12:39:32 -0600

    process.argv

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const mdLinks = require('./mdLinks'); // Importa la función mdLinks desde el archivo mdLinks.js
-const filePath = 'README.md'; // Ruta del archivo Markdown a procesar
+const program = require('commander');
+
+program
+  .version('1.0.0')
+  .description('Un CLI para buscar links, status, file de un archivo .md en una ruta ingresada');
+
+program
+  .command('buscar <ruta>')
+  .description('Procesa una ruta')
+  .action((ruta) => {
+    console.log('Ruta proporcionada:', ruta);
+   
+  
+const filePath = ruta; // Ruta del archivo Markdown a procesar, la ingresa el usuario (node index.js procesar archivo.md)
 
 mdLinks(filePath, { validate: true }) // Llama a la función mdLinks con la opción de validación habilitada
   .then((links) => {
@@ -9,3 +22,7 @@ mdLinks(filePath, { validate: true }) // Llama a la función mdLinks con la opci
   .catch((error) => {
     console.error('Error:', error); // Maneja errores e imprime un mensaje de error en la consola
   });
+
+});
+program.parse(process.argv);
+/*  se utiliza para tomar los argumentos de la línea de comandos que están en process.argv y procesarlos de acuerdo a las definiciones previamente establecidas en programa utilizando commander  */
